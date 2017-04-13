a78662d6ed62d78804fd4649d7c97569bbf61144
Sydnie Fenske <sydniefenske@Sydnies-MacBook-Pro.local>
2017-04-13 13:24:09 -0700

    Initial Commit

---
+++ ExtDomainModel/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ExtDomainModel
+//
+//  Created by Sydnie Fenske on 4/13/17.
+//  Copyright © 2017 Sydnie Fenske. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
